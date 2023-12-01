89f98512c7594fab7be942c0a4b583c714a4baf8
ViktorDojchinovski <viktorjano@yahoo.com>
2023-12-01 19:19:04 +0100

    initial commit

---
+++ project_structure.md
@@ -0,0 +1,19 @@
+my-webpack-project/
+|-- public/
+| |--index.html
+|-- src/
+| |-- js/
+| | |-- index.js // Entry point for JavaScript
+| | |-- carousel.js // JavaScript for the image carousel
+| | |-- tabs.js // JavaScript for the tabs list
+| |-- scss/
+| | |-- styles.scss // Entry point for SCSS
+| | |-- \_carousel.scss // SCSS for carousel styling
+| | |-- \_tabs.scss // SCSS for tabs styling
+|-- dist/
+| |-- index.html // Output HTML file
+| |-- app.bundle.js // Output JavaScript bundle
+| |-- styles.bundle.css // Output CSS bundle
+|-- webpack.config.js // Webpack configuration file
+|-- package.json
+|-- package-lock.json
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "index.html",
+    }),
+  ],
+  devServer: {
+    port: 9000,
+    compress: true,
+    hot: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+    contentBase: path.join(__dirname, "public"),
+  },
+};
